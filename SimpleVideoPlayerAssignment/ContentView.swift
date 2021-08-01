a9b3d1c6ec3b1a24d4b368c13ab7a5e78660bcbe
Lukáš Schmelcer <xschme00@stud.fit.vutbr.cz>
2021-08-01 17:59:05 +0200

    Initial Commit

---
+++ SimpleVideoPlayerAssignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleVideoPlayerAssignment
+//
+//  Created by Lukas Schmelcer on 01/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleVideoPlayerAssignment/SimpleVideoPlayerAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleVideoPlayerAssignmentApp.swift
+//  SimpleVideoPlayerAssignment
+//
+//  Created by Lukas Schmelcer on 01/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SimpleVideoPlayerAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
